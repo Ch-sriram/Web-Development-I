0c04bd875e5a8b1c24c19cbccbb1d46853dab9b4
Sriram Chandrabhatta <sriram.ch1995@gmail.com>
2019-06-02 03:02:21 +0530

    #31: Added operators_1.html & operator_1.js

---
+++ Basics of JavaScript/scripts/operators_1.js
@@ -0,0 +1,11 @@
+var a = 7
+var expr1 = (++a) * 3;
+
+document.write("When a = 7, (++a) * 3 = " + expr1); // O/P: 24
+
+a--;
+var expr2 = (a++) * 3;
+document.write(" & (a++) * 3 = " + expr2); // O/P: 21
+
+a = a * 10 - 4; 
+document.write("<br/> a = a * 10 - 4; // makes a = " + a); // O/P: 76
